426991fe0569ec140729d0890ea68a270e71a755
ResilioSyncZh <97349563+ResilioSyncZh@users.noreply.github.com>
2024-10-01 16:01:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-01-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: '#' },
-    { title: '获取更多帮助', url: '#' }
+    { title: '欢迎来到我的福利爸，你可以在这里成长', url: '#' },
+    { title: '加入福利社群', url: 'https://t.me/fulipro1' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2023/06/02/14/11/woman-8035752_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
@@ -112,7 +112,7 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流群',
+  HEO_SOCIAL_CARD_TITLE_1: '加入交流群',
   HEO_SOCIAL_CARD_TITLE_2: '加入电报群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入',
   HEO_SOCIAL_CARD_URL: 'https://t.me/fulipro1',
